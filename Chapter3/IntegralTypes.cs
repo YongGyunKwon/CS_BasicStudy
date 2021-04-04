132f3fe0a13140e2f8e02a8e571613408f490adb
YongGyunKwon <ygkwon50@gmail.com>
2021-04-04 18:15:42 +0900

    add Chatper4

--- Chapter3/IntegralTypes.cs
+++ Chapter3/IntegralTypes.cs
@@ -5,28 +5,28 @@ namespace Chapter3
     class IntegralTypes
     {
         
-        static void Main(string[] args)
-        {
-            sbyte a=-10;
-            byte b=40;
+        // static void Main(string[] args)
+        // {
+        //     sbyte a=-10;
+        //     byte b=40;
 
-            Console.WriteLine("a={0}, b={1}",a,b);
+        //     Console.WriteLine("a={0}, b={1}",a,b);
 
-            short c = -3000;
-            ushort d =6000;
+        //     short c = -3000;
+        //     ushort d =6000;
 
-            Console.WriteLine("c={0}, d={1}",c,d);
+        //     Console.WriteLine("c={0}, d={1}",c,d);
 
-            int e=-10000000; //0이 7개
-            uint f=300000000; //0이 8개 
+        //     int e=-10000000; //0이 7개
+        //     uint f=300000000; //0이 8개 
 
-            Console.WriteLine("e={0}, f={1}",e,f);
+        //     Console.WriteLine("e={0}, f={1}",e,f);
 
-            long g = -500000000000; //0 11개
-            ulong h =2000000000000000000; //0이 18개
+        //     long g = -500000000000; //0 11개
+        //     ulong h =2000000000000000000; //0이 18개
 
-            Console.WriteLine("g={0} h={1}",g,h);
-        }
+        //     Console.WriteLine("g={0} h={1}",g,h);
+        // }
         
     }
 }
---
+++ Chapter4/Operater.cs
@@ -0,0 +1,24 @@
+using System;
+
+namespace Chapter4
+{
+    class Operater
+    {
+        // static void Main(String[] args)
+        // {
+        //     int a = 111+222;
+        //     Console.WriteLine("a:{0}",a);
+
+        //     int b = -100;
+        //     Console.WriteLine("b:{0}",b);
+            
+        //     int c = b*10;
+        //     Console.WriteLine("c:{0}",c);
+
+        //     double d =c/6.3;
+        //     Console.WriteLine("d: {0}",d);
+
+        //     Console.WriteLine("22/7 = {0}({1})",22/7,22%7);
+        // }
+    }
+}
